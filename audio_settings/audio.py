dce45b769af428fb2102ff8b77fe9e88e31b6a22
Mukhammadkodir Abdusalomov <99057791+Mukhammadkodir27@users.noreply.github.com>
2024-06-08 16:56:20 +0200

    open webbrowser extension added

--- audio_settings/audio.py
+++ audio_settings/audio.py
@@ -12,6 +12,11 @@ engine.setProperty("voices", voices[1].id)  # 0 - male, 1 - female
 # whenever computer hears this word, AI Assistant activates
 activationWord = "computer"
 
+# Configure browser
+# Set the path
+chrome_path = r"C:\Program Files\Google\Chrome\Application\chrome.exe"
+webbrowser.register("chrome", None, webbrowser.BackgroundBrowser(chrome_path))
+
 
 def speak(text, rate=120):
     engine.setProperty("rate", rate)
@@ -24,7 +29,7 @@ def parseCommand():
     print("Listening for a command...")
 
     with sr.Microphone() as source:
-        listener.pause_threshold = 2
+        listener.pause_threshold = 1
         input_speech = listener.listen(source)
 
     try:
@@ -42,7 +47,7 @@ def parseCommand():
 
 
 if __name__ == "__main__":
-    speak("towardsmomentum")
+    speak("Activated...")
 
     while True:
         # Parse as a list
@@ -54,8 +59,14 @@ if __name__ == "__main__":
             # List commands
             if query[0] == "say":
                 if "hello" in query:
-                    speak("Hello!")
+                    speak("Hello! My Boss!")
                 else:
-                    query.pop(0)
+                    query.pop(0)  # remove say
                     speech = " ".join(query)
                     speak(speech)
+
+            # Navigation
+            if query[0] == "go" and query[1] == "to":
+                speak("Opening...")
+                query = " ".join(query[2:])
+                webbrowser.get("chrome").open_new(query)
